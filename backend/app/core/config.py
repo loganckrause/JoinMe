6e1d03c91da96b11c77b1318bdc688c9ce8f3d39
Logan Krause <loganckrause@outlook.com>
2026-04-01 01:03:19 -0400

    backend modifications

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,9 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    DATABASE_URL: str = "mysql+pymysql://joinme:joinme_password@localhost:3306/joinme"
     SECRET_KEY: str = "secret-key"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    API_HOST: str = "0.0.0.0"
+    API_PORT: int = 8000
 
     model_config = SettingsConfigDict(env_file="../.env")
 
