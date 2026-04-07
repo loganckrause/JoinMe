6308a45da78cd28505bd9cb83a373a5ee6a22319
Sofia Borukhovich <117390927+SBorukhovich@users.noreply.github.com>
2026-04-07 12:37:52 -0400

    All Events page and minor bug/layout fixes (#12)
    * feed empty state & event queue refactor
    
    * update sidebar menu items to reflect active page.
    
    * Improve topbar layout in feed and user profile to be same
    
    * Create EventList component and use it to create All Events Page.

---
+++ frontend/components/ui/event-list.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity, Pressable } from "react-native";
+import { IconSymbol } from "./icon-symbol.ios";
+import { ThemedView } from "../themed-view";
+
+type Event = {
+  title: string;
+  number: string;
+  location: string;
+  image: string;
+  description: string;
+};
+
+type Props = {
+  events: Event[];
+};
+
+export default function EventList({ events }: Props) {
+  return (
+    
+    <View style={styles.container}>
+      {events.map((event) => (
+          <Pressable
+              key={event.number}
+              onPress={() => console.log(event.title)}
+              style={({ pressed }) => [
+                styles.eventCard,
+                pressed && { opacity: 0.7 }
+              ]}
+          >
+            <View style={styles.left}>
+              <Text style={styles.eventTitle} numberOfLines={1}>
+                {event.title}
+              </Text>
+
+              <Text style={styles.eventText} numberOfLines={1}>
+                {event.location}
+              </Text>
+
+              <ThemedView style={styles.row}>
+                <Text style={styles.eventPeople}>{event.number}</Text>
+                <IconSymbol size={25} name="person.2" color="#fff" />
+              </ThemedView>
+            </View>
+            
+            <Image source={{ uri: event.image }} style={styles.eventImage} />
+          </Pressable>
+        ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    toggle: {
+        backgroundColor: "#6b2424",
+    },
+   
+  container: {
+    gap: 15,
+  },
+
+  eventCard: {
+    flexDirection: "row",            
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 12,
+    borderWidth: 1,
+    borderColor: "#868585",
+    borderRadius: 12,
+    backgroundColor: "#0f0f0f",
+  },
+
+  left: {
+    flex: 1,                       
+    gap: 6,
+    paddingRight: 10,              
+  },
+
+  eventTitle: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#fff",
+  },
+
+  eventText: {
+    fontSize: 14,
+    color: "#aaa",
+  },
+
+  eventImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 10,
+  },
+    eventPeople: {
+    fontSize: 18,
+    color: "#aaa",
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 5,
+    backgroundColor: 'transparent',
+  },
+  
+  
+});
